e940075768c776343bbdd914d437900957b4d4e4
reezeKim <reezekim.kr@gmail.com>
2024-07-02 18:31:30 +0900

    ✨ Implement function to toggle content and active button

--- js/global.js
+++ js/global.js
@@ -1,9 +1,23 @@
 $(function(){
   const $gnb = $('.gnb');
   const $gnbHeight = $gnb.height();
+  const $toggleBtn = $('.toggle-box__btn');
+  const $toggleContent = $('.toggle-box__content');
 
   function init(){
     mobileGnb();
+    setting();
+    bindEvent();
+  }
+
+  function bindEvent() {
+    $toggleBtn.on('click', toggleContnet);
+  }
+
+  function setting() {
+    $toggleBtn.each(function () {
+      $(this).hasClass('active') ? $(this).next().show() : $(this).next().hide();
+    })
   }
 
   function mobileGnb() {
@@ -14,6 +28,18 @@ $(function(){
 
   function bodyCSS() {
     $('body').css('padding-bottom', $gnbHeight + 20);
+  }
+
+  // 토글 콘텐츠
+  function toggleContnet() {
+    // .active 버튼에 존재하면
+    if( $(this).hasClass('active')) {
+      $(this).next().hide();
+      $(this).removeClass('active');
+    }else{
+      $(this).next().show();
+      $(this).addClass('active');
+    }
   }
 
   init();
